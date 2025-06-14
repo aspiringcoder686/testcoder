38ac81bf5614ceef8fc4292a151254e66369f920
aspiringcoder686 <suryadotnet686@gmail.com>
2025-06-14 13:45:15 -0400

    Update nowread.cs

--- nowread.cs
+++ nowread.cs
@@ -1,48 +1,52 @@
- NumberOfLines = File.ReadAllLines(file).Length
+Directory.CreateDirectory(Path.Combine(outputPath, "Constants"));
+var constantsCode = ConstantGenerator.GenerateConstants(allEntities);
+File.WriteAllText(Path.Combine(outputPath, "Constants", "QueryMapConstants.cs"), constantsCode);
 
-  var methodRegex = new Regex(@"(public|private|protected|internal)\s+(static\s+)?\S+\s+(?<name>\S+)\s*\(.*?\)\s*{");
-                var ajaxRegex = new Regex(@"\[AjaxMethod\]", RegexOptions.IgnoreCase);
-                var paramRegex = new Regex(@"Request\.(QueryString|Params|Form|Cookies|ServerVariables)", RegexOptions.IgnoreCase);
 
-                var methodMatches = methodRegex.Matches(content);
 
-                foreach (Match m in methodMatches)
-                {
-                    string methodName = m.Groups["name"].Value;
+public static class ConstantGenerator
+{
+    public static string GenerateConstants(List<EntityDefinition> entities)
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine("namespace YourNamespace.Constants");
+        sb.AppendLine("{");
+        sb.AppendLine("    public static class QueryMapConstants");
+        sb.AppendLine("    {");
 
-                    var beforeMethod = content.Substring(0, m.Index);
-                    bool isAjax = ajaxRegex.Matches(beforeMethod).Count > 0;
+        foreach (var entity in entities)
+        {
+            sb.AppendLine($"        public static class {entity.Name}Constants");
+            sb.AppendLine("        {");
 
-                    bool usesParam = false;
-                    int numLines = 0;
+            var uniqueQueries = entity.Queries
+                .GroupBy(q => q.Name)
+                .Select(g => g.First());
 
-                    int braceIndex = content.IndexOf('{', m.Index);
-                    if (braceIndex >= 0)
-                    {
-                        int level = 1;
-                        int i = braceIndex + 1;
-                        while (i < content.Length && level > 0)
-                        {
-                            if (content[i] == '{') level++;
-                            else if (content[i] == '}') level--;
-                            i++;
-                        }
-
-                        if (i > braceIndex)
-                        {
-                            var methodBody = content.Substring(braceIndex, i - braceIndex);
-                            usesParam = paramRegex.IsMatch(methodBody);
-                            numLines = methodBody.Split('\n').Length;
-                        }
-                    }
-
-                    methodDetails.Add(new MethodDetail
-                    {
-                        FileName = Path.GetFileName(file),
-                        MethodName = methodName,
-                        IsAjaxMethod = isAjax ? "Yes" : "No",
-                        UsesParameter = usesParam ? "Yes" : "No",
-                        NumberOfLines = numLines
-                    });
-                }
+            foreach (var query in uniqueQueries)
+            {
+                string constName = NormalizeName(query.Name);
+                sb.AppendLine($"            public const string {constName} = \"{query.Name}\";");
             }
+
+            sb.AppendLine("        }");
+        }
+
+        sb.AppendLine("    }");
+        sb.AppendLine("}");
+
+        return sb.ToString();
+    }
+
+    private static string NormalizeName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name)) return "Unknown";
+        // Convert to PascalCase, remove invalid chars if needed
+        return name.Replace(".", "")
+                   .Replace("-", "")
+                   .Replace(" ", "")
+                   .Replace("_", "")
+                   .Replace("(", "")
+                   .Replace(")", "");
+    }
+}
