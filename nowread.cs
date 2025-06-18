a535cf9322fa583085b0ca4ff6855b77c0367f4d
aspiringcoder686 <suryadotnet686@gmail.com>
2025-06-18 16:55:55 -0400

    Update nowread.cs

--- nowread.cs
+++ nowread.cs
@@ -1,22 +1,20 @@
 protected void Application_BeginRequest(object sender, EventArgs e)
 {
-    var url = HttpContext.Current.Request.Url.AbsolutePath;
-    var method = HttpContext.Current.Request.HttpMethod;
+    var request = HttpContext.Current.Request;
+    var url = request.Url.AbsolutePath;
+    var query = request.QueryString.ToString();
+    var method = request.HttpMethod.ToUpper();  // GET, POST, etc.
 
-    var queryString = HttpContext.Current.Request.QueryString.ToString();
-    log4net.ThreadContext.Properties["Url"] = url;
-    log4net.ThreadContext.Properties["HttpMethod"] = method;
-    log4net.ThreadContext.Properties["Query"] = queryString;
+    var fullUrl = !string.IsNullOrEmpty(query) ? $"{url}?{query}" : url;
+
+    // Set it in the ThreadContext as: [GET: /path?qs]
+    log4net.ThreadContext.Properties["RequestInfo"] = $"{method}: {fullUrl}";
 }
 
 protected void Application_EndRequest(object sender, EventArgs e)
 {
-    log4net.ThreadContext.Properties.Remove("Url");
-    log4net.ThreadContext.Properties.Remove("HttpMethod");
-    log4net.ThreadContext.Properties.Remove("Query");  // If you added query string logging
+    log4net.ThreadContext.Properties.Remove("RequestInfo");
 }
 
-<conversionPattern value="--- Starts query ---%newline%date %-5level %logger - [Url: %property{Url}] [HttpMethod: %property{HttpMethod}] %message%newline--- Ends query ---%newline" />
 
-
-    [Query: %property{Query}]
+<conversionPattern value="%date %-5level %logger - [%property{RequestInfo}] %message%newline" />
