7d328bd29f4ca9bc4d99cd4ec708203382846b7f
aspiringcoder686 <suryadotnet686@gmail.com>
2025-09-11 03:37:45 +0530

    Update nowread.cs

--- nowread.cs
+++ nowread.cs
@@ -1,93 +1,29 @@
-public class AmBusinessRuleSet
-{
-    public Guid BusinessRuleSetGuid { get; set; }
-    public Guid TemplateGuid { get; set; }
-    public bool Active { get; set; }
-    public bool Final { get; set; }
-    public string Nickname { get; set; }
-    public string BusinessRuleSetXml { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public string CreatedBy { get; set; }
-    public DateTime LastModifiedDate { get; set; }
-    public string LastModifiedBy { get; set; }
-
-    // 🔑 Add navigation
-    public virtual ICollection<AmBusinessRule> BusinessRules { get; set; } = new List<AmBusinessRule>();
-}
-
-public class AmBusinessRule
-{
-    public Guid BusinessRuleGuid { get; set; }
-    public Guid BusinessRuleSetGuid { get; set; }
-    public Guid? BusinessRuleSurrogateGuid { get; set; }
-    public string Code { get; set; }
-    public string Name { get; set; }
-    public string Formula { get; set; }
-    public string ErrorMessage { get; set; }
-    public int Severity { get; set; }
-
-    // 🔑 Navigation to parent set
-    public virtual AmBusinessRuleSet BusinessRuleSet { get; set; }
-
-    // 🔑 Navigations for links
-    public virtual ICollection<AmBusinessRuleFoundGroupOverride> FundGroupOverrides { get; set; } = new List<AmBusinessRuleFoundGroupOverride>();
-    public virtual ICollection<AmBusinessRuleModelType> ModelTypeLinks { get; set; } = new List<AmBusinessRuleModelType>();
-}
-
-public static Expression<Func<AmBusinessRuleSet, Domain.BusinessRuleSet.BusinessRuleSet>> Expression =>
-    src => new Domain.BusinessRuleSet.BusinessRuleSet
+var set = await ctx.AmBusinessRuleSets
+    .Where(s => s.BusinessRuleSetGuid == guid)
+    .Select(s => new BusinessRuleSet
     {
-        Id                   = src.BusinessRuleSetGuid,
-        TemplateGUID         = src.TemplateGuid,
-        Active               = src.Active,
-        Final                = src.Final,
-        Nickname             = src.Nickname,
-        BusinessRuleSetXML   = src.BusinessRuleSetXml,
-        LastUpdatedTimestamp = src.LastModifiedDate,
-        LastModifiedBy       = src.LastModifiedBy,
-        CreatedTimestamp     = src.CreatedDate,
-        CreatedBy            = src.CreatedBy,
-
-        BusinessRules = src.BusinessRules.Select(a => new Domain.BusinessRule.BusinessRule
-        {
-            Id                      = a.BusinessRuleGuid,
-            Code                    = a.Code,
-            Name                    = a.Name,
-            Formula                 = a.Formula,
-            ErrorMessage            = a.ErrorMessage,
-            Severity                = a.Severity,
-            BusinessRuleSurrogateId = a.BusinessRuleSurrogateGuid,
-
-            FundGroupsOverride = a.FundGroupOverrides
-                .Where(fg => fg.FundGroup != null)
-                .Select(fg => new Domain.FundGroup
-                {
-                    ShortName = fg.FundGroup.FundGroupShortName,
-                    LongName  = fg.FundGroup.LongName
-                }).ToList(),
-
-            ModelTypes = a.ModelTypeLinks
-                .Where(mt => mt.AssetModelType != null)
-                .Select(mt => new Domain.AssetModelType
-                {
-                    Code           = mt.AssetModelType.AssetModelTypeCode,
-                    Description    = mt.AssetModelType.AssetModelTypeDesc,
-                    UploadTypeCode = mt.AssetModelType.UploadTypeCode,
-
-                    ModelUploadType = new Domain.Upload.ModelUploadType
-                    {
-                        Code                = mt.AssetModelType.UploadTypeCodeNavigation.UploadTypeCode,
-                        Description         = mt.AssetModelType.UploadTypeCodeNavigation.UploadTypeDesc,
-                        DisplayOrder        = mt.AssetModelType.UploadTypeCodeNavigation.DisplayOrder,
-                        FinalizationRequired= mt.AssetModelType.UploadTypeCodeNavigation.FinalizationRequired,
-
-                        UploadCategory = new Domain.Upload.ModelUploadCategory
-                        {
-                            Code            = mt.AssetModelType.UploadTypeCodeNavigation.UploadCategoryCodeNavigation.UploadCategoryCode,
-                            Description     = mt.AssetModelType.UploadTypeCodeNavigation.UploadCategoryCodeNavigation.UploadCategoryDesc,
-                            ContentBaseType = mt.AssetModelType.UploadTypeCodeNavigation.UploadCategoryCodeNavigation.ContentBaseType
-                        }
-                    }
-                }).ToList()
-        }).ToList()
-    };
+        Id = s.BusinessRuleSetGuid,
+        Nickname = s.Nickname,
+        BusinessRules = ctx.AmBusinessRules
+            .Where(r => r.BusinessRuleSetGuid == s.BusinessRuleSetGuid)
+            .Select(r => new BusinessRule
+            {
+                Code = r.Code,
+                Name = r.Name,
+                FundGroupsOverride = ctx.AmBusinessRuleFoundGroupOverrides
+                    .Where(o => o.BusinessRuleSurrogateGuid == r.BusinessRuleSurrogateGuid)
+                    .Join(ctx.AmRefFundGroups,
+                          o => o.FundGroupShortName,
+                          fg => fg.FundGroupShortName,
+                          (o, fg) => new FundGroup { ShortName = fg.FundGroupShortName })
+                    .ToList(),
+                ModelTypes = ctx.AmBusinessRuleModelTypes
+                    .Where(o => o.BusinessRuleSurrogateGuid == r.BusinessRuleSurrogateGuid)
+                    .Join(ctx.AmRefAssetModelTypes,
+                          o => o.AssetModelTypeCode,
+                          mt => mt.AssetModelTypeCode,
+                          (o, mt) => new AssetModelType { Code = mt.AssetModelTypeCode })
+                    .ToList()
+            }).ToList()
+    })
+    .SingleOrDefaultAsync();
